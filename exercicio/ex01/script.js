36d3bf9c08a85d7933309fbeb8ff21d8f5c7cd73
jaelps <jaelsonpsilva18@gmail.com>
2023-03-12 11:15:14 -0300

    ok

---
+++ exercicio/ex01/script.js
@@ -0,0 +1,19 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 15
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12){
+        img.src = 'imagem/manha-trans.png'
+        document.body.style.background = '#ffe457'
+    } else if (hora >= 12 && hora < 18){
+        img.src = 'imagem/tarde-trans.png'
+        document.body.style.background = '#a75320'
+    } else {
+        img.src = 'imagem/noite-trans.png'
+        document.body.style.background = '#171f3c'
+    }
+}
+
