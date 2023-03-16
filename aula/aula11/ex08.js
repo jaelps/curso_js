6671ed7266ff14e12ac939338dcb3168e0e59b3c
jaelps <jaelsonpsilva18@gmail.com>
2023-03-16 09:29:00 -0300

    efetuado exercicios

---
+++ aula/aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 15
+console.log(`A velocidade do carro é ${vel}`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade peritida. Multado`)
+}
+
+console.log('Dirija sempre com cinto de segurança!')
